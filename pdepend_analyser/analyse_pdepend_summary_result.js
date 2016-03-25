fe42c624fb083c5582d69c042391b3c85ef454da
applewu <wuziteng2006@163.com>
2016-03-25 20:39:05 +0800

    update the format of output

--- pdepend_analyser/analyse_pdepend_summary_result.js
+++ pdepend_analyser/analyse_pdepend_summary_result.js
@@ -13,7 +13,7 @@ doc.metrics.package.forEach(function(my_package) {
 		if (temp_class_len != "undefined") {
 			my_package['class'].forEach(
 				function(my_class) {
-					print(my_class['@name'] + ",,,");
+					// print(my_class['@name'] + ",,,");
 					var temp_method = typeof my_class['method']
 						// Check if the 'method' field had existed
 					if (temp_method == "object") {
@@ -21,19 +21,19 @@ doc.metrics.package.forEach(function(my_package) {
 							// Check if the 'method' filed is array; true means 'array', false means k-v object
 						if (temp_method_len != "undefined") {
 							my_class['method'].forEach(function(my_method) {
-								print("," + my_method['@name'] + "," + my_method['@ccn'] + "," + my_method['@ccn2']);
+								print(my_class['@name'] + "," + my_method['@name'] + "," + my_method['@ccn'] + "," + my_method['@ccn2']);
 							})
 						}
 						// if the 'method' is k-v object, no need 'forEach' function, could get the value directly
 						else {
-							print("," + my_class['method']['@name'] + "," + my_class['method']['@ccn'] + "," + my_class['method']['@ccn2']);
+							print(my_class['@name'] + "," + my_class['method']['@name'] + "," + my_class['method']['@ccn'] + "," + my_class['method']['@ccn2']);
 						}
 					}
 				})
 		}
 		// if the 'class' is k-v object, no need 'forEach' function, could get the value directly 
 		else {
-			print(my_package['class']['@name'] + ",,,");
+			// print(my_package['class']['@name'] + ",,,");
 			var temp_method = typeof my_package['class']['method']
 				// Check if the 'method' field had existed
 			if (temp_method == "object") {
@@ -41,12 +41,12 @@ doc.metrics.package.forEach(function(my_package) {
 					// Check if the 'method' filed is array; true means 'array', false means k-v object
 				if (temp_method_len != "undefined") {
 					my_package['class']['method'].forEach(function(my_method) {
-						print("," + my_method['@name'] + "," + my_method['@ccn'] + "," + my_method['@ccn2']);
+						print(my_package['class']['@name'] + "," + my_method['@name'] + "," + my_method['@ccn'] + "," + my_method['@ccn2']);
 					})
 				}
 				// if the 'method' is k-v object, no need 'forEach' function, could get the value directly
 				else {
-					print("," + my_package['class']['method']['@name'] + "," + my_package['class']['method']['@ccn'] + "," + my_package['class']['method']['@ccn2']);
+					print(my_package['class']['@name'] + "," + my_package['class']['method']['@name'] + "," + my_package['class']['method']['@ccn'] + "," + my_package['class']['method']['@ccn2']);
 				}
 			}
 
